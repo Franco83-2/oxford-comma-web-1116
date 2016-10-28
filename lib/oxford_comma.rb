3fd6ecea121c4fdf72bd8d6e6aa8da9ef6038e2d
Franco Cappitta <franco84@gmail.com>
2016-10-28 14:44:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   array[0].to_s
+ elsif array.length == 2
+    array.map { |i| "#{i.to_s}" }.join(" and ")
+  else
+    counter = 0
+    string = ""
+    while counter < (array.length - 1)
+      string << "#{array[counter].to_s}, "
+      counter += 1
+    end
+    string << "and #{array.last.to_s}"
+    string
+ end
 end
